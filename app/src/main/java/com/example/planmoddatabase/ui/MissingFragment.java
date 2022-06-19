2bc7fca7ebfcf148345326f8ff4f5e8f41f1c5c0
ChrzaszczykKamil <Kamil.Chrzaszczyk@student.uj.edu.pl>
2022-06-19 10:43:36 +0200

    style changes

--- app/src/main/java/com/example/planmoddatabase/ui/MissingFragment.java
+++ app/src/main/java/com/example/planmoddatabase/ui/MissingFragment.java
@@ -28,22 +28,22 @@ public class MissingFragment extends Fragment {
 
         ArrayList<String> unknownPlans = new ArrayList<>();
         //Do this in a loop over unknown plans
-        unknownPlans.add("Plan: ULtracite Left Arm");
-        unknownPlans.add("Recipe: Healing Salve (Mire)");
-        unknownPlans.add("Plan: ULtracite Left Leg");
-        unknownPlans.add("Plan: ULtracite Right Arm");
-        unknownPlans.add("Plan: ULtracite Right Leg");
-        unknownPlans.add("Plan: ULtracite Torso");
-        unknownPlans.add("Plan: TEMP PLAN #1");
-        unknownPlans.add("Plan: TEMP PLAN #2");
-        unknownPlans.add("Plan: TEMP PLAN #3");
-        unknownPlans.add("Plan: TEMP PLAN #4");
-        unknownPlans.add("Plan: TEMP PLAN #5");
-        unknownPlans.add("Plan: TEMP PLAN #6");
-        unknownPlans.add("Plan: TEMP PLAN #7");
-        unknownPlans.add("Plan: TEMP PLAN #8");
-        unknownPlans.add("Plan: TEMP PLAN #9");
-        unknownPlans.add("Plan: TEMP PLAN #10");
+        unknownPlans.add("PLAN: ULTRACITE LEFT ARM");
+        unknownPlans.add("RECIPE: HEALING SALVE (MIRE)");
+        unknownPlans.add("PLAN: ULTRACITE LEFT LEG");
+        unknownPlans.add("PLAN: ULTRACITE RIGHT ARM");
+        unknownPlans.add("PLAN: ULTRACITE RIGHT LEG");
+        unknownPlans.add("PLAN: ULTRACITE TORSO");
+        unknownPlans.add("PLAN: TEMP PLAN #1");
+        unknownPlans.add("PLAN: TEMP PLAN #2");
+        unknownPlans.add("PLAN: TEMP PLAN #3");
+        unknownPlans.add("PLAN: TEMP PLAN #4");
+        unknownPlans.add("PLAN: TEMP PLAN #5");
+        unknownPlans.add("PLAN: TEMP PLAN #6");
+        unknownPlans.add("PLAN: TEMP PLAN #7");
+        unknownPlans.add("PLAN: TEMP PLAN #8");
+        unknownPlans.add("PLAN: TEMP PLAN #9");
+        unknownPlans.add("PLAN: TEMP PLAN #10");
 
         myList.setAdapter(new AdapterUnknown(unknownPlans, view.getContext()) );
 
