aa0384ee4a6f564148d6e92c607ee63f650b7bc8
ChrzaszczykKamil <Kamil.Chrzaszczyk@student.uj.edu.pl>
2022-06-28 23:30:50 +0200

    added recursive category opening and a few mods

--- app/src/main/java/com/example/planmoddatabase/activities/DatabaseListActivity.java
+++ app/src/main/java/com/example/planmoddatabase/activities/DatabaseListActivity.java
@@ -3,10 +3,10 @@ package com.example.planmoddatabase.activities;
 
 import static com.example.planmoddatabase.activities.MainActivity.DATABASE_MENU;
 
-import android.app.Activity;
-import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.planmoddatabase.R;
 import com.example.planmoddatabase.adaptes.AdapterSection;
@@ -17,10 +17,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class DatabaseListActivity extends Activity {
+public class DatabaseListActivity extends AppCompatActivity {
 
     ListView myList;
-    String fileName;
+    public static String fileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,40 +29,45 @@ public class DatabaseListActivity extends Activity {
         myList = findViewById(R.id.SectionOfTheDatabase);
         ArrayList<String> unknownPlans = new ArrayList<>();
 
-        AssetManager am = this.getAssets();
-
-        if(DATABASE_MENU==R.id.weapons)
-        {
+        if(DATABASE_MENU==R.id.weapons) {
             System.out.println("weapons");
             fileName="weapons";
-        }else if(DATABASE_MENU==R.id.armor) {
+        }
+        else if(DATABASE_MENU==R.id.armor) {
             System.out.println("armor");
             fileName="armor";
-        }else if(DATABASE_MENU==R.id.pa) {
+        }
+        else if(DATABASE_MENU==R.id.pa) {
             System.out.println("pa");
             fileName="pa";
-        }else if(DATABASE_MENU==R.id.camp) {
+        }
+        else if(DATABASE_MENU==R.id.camp) {
             System.out.println("camp");
             fileName="camp";
-        }else if(DATABASE_MENU==R.id.chemistry) {
+        }
+        else if(DATABASE_MENU==R.id.chemistry) {
             System.out.println("chemistry");
             fileName="chemistry";
-        }else if(DATABASE_MENU==R.id.cook) {
-            System.out.println("cook");
-            fileName="cook";
-        }else if(DATABASE_MENU==R.id.tinkers) {
+        }
+        else if(DATABASE_MENU==R.id.cooking) {
+            System.out.println("cooking");
+            fileName="cooking";
+        }
+        else if(DATABASE_MENU==R.id.tinkers) {
             System.out.println("tinkers");
             fileName="tinkers";
-        }else if(DATABASE_MENU==R.id.brewing) {
+        }
+        else if(DATABASE_MENU==R.id.brewing) {
             System.out.println("brewing");
             fileName="brewing";
-        }else if(DATABASE_MENU==R.id.photomode) {
+        }
+        else if(DATABASE_MENU==R.id.photomode) {
             System.out.println("photomode");
             fileName="photomode";
         }
 
         try {
-            InputStream is = am.open(fileName);
+            InputStream is = this.getAssets().open(fileName);
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             String line;
 
@@ -73,14 +78,8 @@ public class DatabaseListActivity extends Activity {
         }
         System.out.println(DATABASE_MENU);
 
-        myList.setAdapter(new AdapterSection(unknownPlans, this));
-
-        /*
-        this thing will be useful
-        val firstAdapter: FirstAdapter = ;
-        val secondAdapter: SecondAdapter = ;
-        val thirdAdapter: ThirdAdapter = ;
-        val concatAdapter = ConcatAdapter(firstAdapter, secondAdapter, thirdAdapter);
-        recyclerView.adapter = concatAdapter;*/
+        setTitle(unknownPlans.get(0));
+        unknownPlans.remove(0);
+        myList.setAdapter(new AdapterSection(unknownPlans, this, DatabaseListActivity.this));
     }
 }
--- app/src/main/java/com/example/planmoddatabase/adaptes/AdapterSection.java
+++ app/src/main/java/com/example/planmoddatabase/adaptes/AdapterSection.java
@@ -1,5 +1,7 @@
 package com.example.planmoddatabase.adaptes;
 
+import static com.example.planmoddatabase.activities.DatabaseListActivity.fileName;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,19 +9,27 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import com.example.planmoddatabase.R;
+import com.example.planmoddatabase.activities.DatabaseListActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class AdapterSection extends BaseAdapter implements ListAdapter {
 
-    private final ArrayList<String> list;
+    private ArrayList<String> list;
     private final Context context;
+    DatabaseListActivity parentActivity;
 
-    public AdapterSection(ArrayList<String> list, Context context) {
+    public AdapterSection(ArrayList<String> list, Context context, DatabaseListActivity parentActivity) {
         this.list = list;
         this.context = context;
+        this.parentActivity=parentActivity;
     }
 
     @Override
@@ -42,18 +52,53 @@ public class AdapterSection extends BaseAdapter implements ListAdapter {
     public View getView(final int position, View convertView, ViewGroup parent) {
 
         View view = convertView;
-        if (view == null) {
+        String[] listEntry = list.get(position).split(" ", 2);
+        if(listEntry[0].equals("--")) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.row_dropdown_menu, null);
+
+            Button Item = view.findViewById(R.id.itemX);
+            Item.setText(listEntry[1].split(" ", 2)[1]);
+
+            Item.setOnClickListener(v -> {
+                ArrayList<String> unknownPlans = new ArrayList<>();
+                fileName+="Sub/"+listEntry[1].split(" ", 2)[0];
+                try {
+                    InputStream is = context.getAssets().open(fileName);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        unknownPlans.add(line);
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //TODO: Recursive sublists loading
+                list =unknownPlans;
+                parentActivity.setTitle(unknownPlans.get(0));
+                list.remove(0);
+                notifyDataSetChanged();
+
+                System.out.println("opening"+fileName);
+            });
+        }else if(listEntry[0].equals("..")) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.row_unknown, null);
+            TextView planName= view.findViewById(R.id.planName);
+            planName.setText(listEntry[1]);
+        }else if(listEntry[0].equals("++")) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.row_unknown, null);
+            TextView planName= view.findViewById(R.id.planName);
+            planName.setText("BASE "+listEntry[1]);
+        }else if(listEntry[0].equals("||")) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.row_unknown, null);
+            TextView planName= view.findViewById(R.id.planName);
+            planName.setText("--"+listEntry[1]);
         }
-
-        Button Item= view.findViewById(R.id.itemX);
-        Item.setText(list.get(position));
-
-        Item.setOnClickListener(v -> {
-            //TODO: Recursive sublists loading
-            System.out.println(list.get(position));
-        });
 
         return view;
     }
